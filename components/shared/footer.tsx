15c29f8f09cc16a23c8bbc721cce5286362b2b72
akshay-kakade <akshaykakade648@gmail.com>
2025-03-01 13:35:02 +0530

    Ui fixx

--- components/shared/footer.tsx
+++ components/shared/footer.tsx
@@ -4,6 +4,8 @@ import { ChevronUp } from "lucide-react";
 import { Button } from "../ui/button";
 import Link from "next/link";
 import { APP_NAME } from "@/lib/constants";
+import { socials } from "@/lib/data";
+import Image from "next/image";
 
 export default function Footer() {
 
@@ -29,13 +31,27 @@ export default function Footer() {
             </div>
             <div className="flex justify-center text-sm">
                 <p>© 2025 {APP_NAME} PTV LTD.</p>
-
+              
             </div>
-            <div className="mt-8 justify-center text-sm text-black">
-            Newasa Rd, Shrirampur, Siddharth Nagar, Maharashtra 413709,
-            <br />
-             Call : +91 1444354645 ,
-            email : swiftstore@gmail.com
+            <div>
+            <ul className="flex flex-1 justify-center gap-3"> 
+             {socials.map(({id, url, icon, title }) => (
+                    <li key={id}>
+                        <a href={url} className="social-icon">
+                            <Image 
+                                src={icon}
+                                alt={title}
+                                className="size-6 object-contain hover:rotate-12"
+                                width={24}
+                                height={24}
+                            />
+                        </a>
+                    </li>
+             ))}
+        </ul>
+            </div>
+            <div className="m5-8 justify-center text-sm text-black text-center">
+            Newasa Rd, Shrirampur, Siddharth Nagar, Maharashtra 413709, Call : +91 1444354645, email : swiftstore@gmail.com
             </div>
            </div>
         </footer>
--- lib/data.ts
+++ lib/data.ts
@@ -33,6 +33,35 @@ const data = {
      },
 ], 
 
+
+
 }
 
 export default data;
+
+export const socials = [
+   {
+     id: '0',
+     title: 'Youtube',
+     icon: '/youtube.svg',
+     url: 'https://www.youtube.com/@itsgametimebudy',
+   },
+   {
+     id: '1',
+     title: 'GitHub',
+     icon: '/github.svg',
+     url: 'https://github.com/akshay-kakade',
+   },
+   {
+     id: '2',
+     title: 'Instagram',
+     icon: '/instagram.svg',
+     url: 'https://www.instagram.com/maverick_7821/?igsh=MTJiN3M3NDNjM2JkcA%3D%3D',
+   },
+   {
+     id: '3',
+     title: 'Linkdin',
+     icon: '/linkedin.svg',
+     url: 'https://www.linkedin.com/in/akshay-kakade-878399287/?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app',
+   },
+ ];
