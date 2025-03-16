ca3b05da613e1af08a5278dcb8c577c249fa0aff
akshay-kakade <akshaykakade648@gmail.com>
2025-03-16 10:58:43 +0530

    vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import ClientProviders from "@/components/shared/client-provider";
 import NextTopLoader from 'nextjs-toploader';
+import { Analytics } from "@vercel/analytics/react"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -30,6 +31,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
        <ClientProviders> <NextTopLoader /> {children} </ClientProviders>
+       <Analytics />
       </body>
     </html>
   );
